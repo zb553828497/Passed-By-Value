c2edd2fb066c60ced10b7e7f0a90f15b9661cb13
zb <zb@163.com>
2016-06-16 20:27:11 +0800

    解释了 调用block时,block的名字是什么

--- Passed-By-Value/Block传值/TenVc.m
+++ Passed-By-Value/Block传值/TenVc.m
@@ -30,6 +30,7 @@
 - (IBAction)PassValue:(id)sender {
     NSArray *array = [NSArray arrayWithObjects:[UIColor redColor],[UIColor purpleColor],[UIColor cyanColor],[UIColor blueColor],nil];
     // 调用myBlock，也就是调用DoneBlock，并传递两个参数到代码块中，然后执行DoneBlock中保存的代码块
+    // 调用block。注意:block的名字本质是initWithBlock:方法的参数
     self.myBlock([array objectAtIndex:rand()%4],self.Value.text);
     [self.navigationController popViewControllerAnimated:YES];
 }
